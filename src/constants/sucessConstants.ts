c77688822777671387b588871e9d50e019b12ede
hamzamaimi <hamzamaimi0901@gmail.com>
2025-03-04 21:44:07 +0100

    chore: removed unused constat

--- src/constants/sucessConstants.ts
+++ src/constants/sucessConstants.ts
@@ -1,5 +1,4 @@
 export const ADMIN_USER_CREATED = 'Admin user created successfully.';
 export const USER_CREATED = 'User created successfully.';
-export const LOGIN_SUCCESSFUL = 'Login correctly done.';
-export const RESET_PASSWORD_SUCCESSFUL = 'If an account with this email exists, a password reset link will be sent.';
+export const RESET_PASSWORD_SUCCESSFUL = 'If an account with this email exists, a temporary password will be sent.';
 export const PASSWORD_CORRECTLY_CHANGED = 'You have correctly changed your password.';
